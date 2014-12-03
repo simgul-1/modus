42054b2e2595bf42b686842afcc5042a947d585c
Victor Ohlsson <vicohl-1@student.ltu.se>
2014-12-03 15:10:51 +0100

    testing server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // server.js
-
+//GITHUB TESTJÄVEL
 // set up ======================================================================
 // get all the tools we need
 var express  = require('express');
