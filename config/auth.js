d2805600e51b4dbd09fd70e01a14cb01157f04dc
simgul-1 <simgul-1@student.ltu.se>
2014-12-05 13:41:52 +0100

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,14 +4,7 @@
 
 // ---------------------- THE REAL DEAL ---------------------------
 // ---------- kommentera ut erat innan ni commitar och pushar!!! ---------------------
-<<<<<<< HEAD
-// module.exports = {
 
-// 	'facebookAuth' : {
-// 		'clientID' 		: '755027641218361', // your App ID
-// 		'clientSecret' 	: '1efc2e1c22ca3198be3d32b04db2aa00', // your App Secret
-// 		'callbackURL' 	: 'http://wayland.campus.ltu.se:3000/auth/facebook/callback'
-=======
 
 module.exports = {
 
@@ -29,25 +22,9 @@ module.exports = {
 
 };
 
-// // SIMONS GREJER
-// module.exports = {
 
-// 	'facebookAuth' : {
-// 		'clientID' 	: '755401647847627', // your App ID
-// 		'clientSecret' 	: 'c34694134dc78bf83523b4cf6eca4f0c', // your App Secret
-// 		'callbackURL' 	: 'http://localhost:3000/auth/facebook/callback'
->>>>>>> FETCH_HEAD
-// 	},
-
-// 	'googleAuth' : {
-//         'clientID'      : '387127984694-bhb4maaj9di77f48qhbohmi1lljgunqv.apps.googleusercontent.com',
-//         'clientSecret'  : 'KDmuaqNAhqk69ZgXD7TEGS7d',
-//         'callbackURL'   : 'http://wayland.campus.ltu.se:3000/auth/google/callback'
-//     }
-
-// };
-
-// SIMONS GREJER
+/*
+ SIMONS GREJER
 module.exports = {
 
 	'facebookAuth' : {
@@ -64,15 +41,14 @@ module.exports = {
 
 };
 
-
+*/
 /*
-<<<<<<< HEAD
-VICTORs grejer 
-=======
+
+
 //VICTORs grejer 
 module.exports = {
 
->>>>>>> FETCH_HEAD
+
 	'facebookAuth' : {
 		'clientID' 	: '747475465332188', // your App ID
 		'clientSecret' 	: 'bc95afdb4fb1705cb77ca6a389f3d6d0', // your App Secret
@@ -81,18 +57,13 @@ module.exports = {
 	'googleAuth' : {
         'clientID'      : '989951666354-cc28vjs8sp6ls5jo6pns7jf55a6e4jh6.apps.googleusercontent.com',
         'clientSecret'  : 'uNNAMhPAck4hzHOCduvTsY0L',
-<<<<<<< HEAD
-        'callbackURL'   : 'localhost:3000/auth/google/callback'
-    }
-=======
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+	'callbackURL'   : 'http://localhost:3000/auth/google/callback'
     	}
 
->>>>>>> FETCH_HEAD
+
 };
 */
-<<<<<<< HEAD
-=======
+
 
 /* Maxime
 module.exports = {
@@ -109,4 +80,4 @@ module.exports = {
 };
 */
 
->>>>>>> FETCH_HEAD
+
